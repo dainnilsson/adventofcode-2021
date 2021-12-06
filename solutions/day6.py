70bc83486f72a94f706bdf982114690f8ec2ab74
Dain Nilsson <dain@yubico.com>
2021-12-06 09:54:26 +0100

    Use different variable name.

--- solutions/day6.py
+++ solutions/day6.py
@@ -2,7 +2,7 @@ def solve(data, log):
     xs, ys = [0] * 7, [0] * 2
     for v in data.split(","):
         xs[int(v)] += 1
-    for f, t in ((0, 80), (80, 256)):
-        for t in range(f, t):
+    for s, e in ((0, 80), (80, 256)):
+        for t in range(s, e):
             ys[t % 2], xs[t % 7] = xs[t % 7], xs[t % 7] + ys[t % 2]
         yield sum(xs + ys)
